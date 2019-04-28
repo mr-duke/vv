589e62121afcd22139e106ac1d4c85827ff47034
Karl Herzog <Karl.M.Herzog@stud.fh-rosenheim.de>
2019-04-28 16:12:01 +0200

    log4J properties dazu

--- 01reactive/src/main/java/de/thro/inf/reactive/Mitarbeiterverwaltung.java
+++ 01reactive/src/main/java/de/thro/inf/reactive/Mitarbeiterverwaltung.java
@@ -1,19 +1,26 @@
 package de.thro.inf.reactive;
 
+import org.apache.log4j.Logger;
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 
+
 public class Mitarbeiterverwaltung {
+
+    private static final Logger SYSTEM_LOGGER = Logger.getLogger("systemLogger");
+    private static final Logger EVENTS_LOGGER = Logger.getLogger("eventLogger");
 
     private static Mitarbeiterverwaltung mitarbeiterverwaltung = null;
     private List<Mitarbeiter> mitarbeiter;
-    private Mitarbeiterverwaltung(){
+    private Mitarbeiterverwaltung() throws IOException {
         mitarbeiter = new LinkedList<>();
     }
 
-    public static Mitarbeiterverwaltung getMitarbeiterverwaltung() {
+    public static Mitarbeiterverwaltung getMitarbeiterverwaltung() throws IOException {
         if (mitarbeiterverwaltung == null)
             mitarbeiterverwaltung = new Mitarbeiterverwaltung();
+        SYSTEM_LOGGER.debug("Mitarbeiterverwaltung gestartet");
         return mitarbeiterverwaltung;
     }
 
@@ -23,6 +30,7 @@ public class Mitarbeiterverwaltung {
             mitarbeiter.add(new Mitarbeiter(e.getMitarbeiterId()));
         else
             m.bewegen(m.getAktuellerZustand(), e.getRichtung());
+        EVENTS_LOGGER.info("Mitarbeiter neu angelegt");
     }
 
     public List<Mitarbeiter> getMitarbeiter() {
