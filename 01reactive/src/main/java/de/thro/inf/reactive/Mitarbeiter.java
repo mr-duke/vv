ed26a687354aa99474c1a801229588d0107fc96e
Karl Herzog <Karl.M.Herzog@stud.fh-rosenheim.de>
2019-05-19 12:03:32 +0200

    Mitarbeiter: zusätzliche Kommentare

--- 01reactive/src/main/java/de/thro/inf/reactive/Mitarbeiter.java
+++ 01reactive/src/main/java/de/thro/inf/reactive/Mitarbeiter.java
@@ -12,11 +12,16 @@ public class Mitarbeiter {
     public enum Zustand {ABWESEND, IMGANG, ANWESEND, ERROR};
     public enum Richtung {LINKS, RECHTS};
 
+    // Zustandüberstandsdiagramm laut Angabe
     private Zustand uebergang [][] = {
             {Zustand.IMGANG, Zustand.ABWESEND,Zustand.ERROR, null },
             {Zustand.ERROR, Zustand.ANWESEND, Zustand.IMGANG, null }
     };
 
+    // Mitarbeiter wird neuer Zustand zugewiesen, je nachdem an welchem Sensor er vorbeiläuft
+    // Falls Mitarbeiter in Zustand.ERROR gelangen sollte, wird Fehlermeldung geworfen
+    // und sein aktuellerZustand wird nicht neu gesetzt bzw. bleibt auf dem vorherigen Zustand!
+    // Fehlermeldung wird geloggt
     public void bewegen(Zustand zustand, Richtung richtung){
         Zustand neuerZustand = uebergang [richtung.ordinal()][zustand.ordinal()];
         if (neuerZustand == Zustand.ERROR)
