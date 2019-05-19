15b6e0ce30afd44ed35d21b83ef2d1b89da9b42d
Karl Herzog <Karl.M.Herzog@stud.fh-rosenheim.de>
2019-05-19 14:29:48 +0200

    Executor zu Server und Client hinzufügen

--- 01reactive/src/main/java/de/thro/inf/reactive/Sensor.java
+++ 01reactive/src/main/java/de/thro/inf/reactive/Sensor.java
@@ -3,7 +3,6 @@ package de.thro.inf.reactive;
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 import configuration.FromConsole;
-import configuration.FromFile;
 import configuration.IConfiguration;
 import org.apache.log4j.Logger;
 
@@ -11,6 +10,8 @@ import java.io.*;
 import java.net.Socket;
 import java.util.Scanner;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class Sensor {
@@ -18,28 +19,32 @@ public class Sensor {
     private static final Logger SYSTEM_LOGGER = Logger.getLogger("systemLogger");
     private static final String SHUTDOWN_KEY = "QUIT";
     private static BlockingQueue<String> outgoing = new LinkedBlockingQueue<>();
-    /* Hier wird Config-Art festgelegt: FromConsole oder FromFile*/
+    private static Executor exec = Executors.newCachedThreadPool();
+
+    // Hier wird Config-Art festgelegt: new FromConsole() oder new FromFile()
+    // Config liest mithilfe von Properties-File nötige Konfigurationsinfos aus
     private static IConfiguration config = new FromConsole();
 
     private SensorArt sensorArt;
-    public enum SensorArt {LINKS, RECHTS};
     private Sensor(SensorArt sensorArt) {
 
         this.sensorArt = sensorArt;
     }
+    public enum SensorArt {LINKS, RECHTS};
 
     public static void main(String[] args) {
         Sensor sensor = new Sensor(config.getSensorArt());
-        Thread clientThread = new Thread(() -> createClientSocket(sensor));
-        clientThread.start();
+        exec.execute(() -> provideClientSocket(sensor));
+        //Thread clientThread = new Thread(() -> provideClientSocket(sensor));
+        //clientThread.start();
 
         Scanner scanner = new Scanner(config.getInputStream());
         while (true){
-            readFromInput(scanner);
+            readFromInputStream(scanner);
         }
     }
 
-    private static void createClientSocket(Sensor sensor) {
+    private static void provideClientSocket(Sensor sensor) {
         Socket socket = null;
         try {
             socket = new Socket(config.getInetAdress(), config.getPortNumber());
@@ -51,19 +56,19 @@ public class Sensor {
             shutdownSensor(socket, sensor);
             return;
         }
-        createOutputStream(sensor, socket);
+        provideOutputStream(sensor, socket);
     }
-    // Name ändern
-    private static void createOutputStream(Sensor sensor, Socket socket) {
+
+    private static void provideOutputStream(Sensor sensor, Socket socket) {
         try (OutputStream output = socket.getOutputStream()) {
 
             while (true) {
                 String jsonString = outgoing.take();
-
+                // Bei Senden von SHUTDOWN_KEY:
+                // Fahre Sensor/Client herunter und sende Info auch an Server zum Entfernen des Client-Socket
                 if (jsonString.equals(SHUTDOWN_KEY)){
                     writeToOutputStream(output, jsonString);
                     shutdownSensor(socket, sensor);
-
                 }
                 writeToOutputStream(output, jsonString);
             }
@@ -76,7 +81,7 @@ public class Sensor {
     private static void writeToOutputStream(OutputStream output, String jsonString)  {
         try {
             output.write(jsonString.getBytes());
-            // Kommentar
+            // Nötig, damit Scanner in Server-Objekt das Zeilenende erkennt
             output.write("\r\n".getBytes());
             output.flush();
         } catch (IOException ex) {
@@ -84,7 +89,8 @@ public class Sensor {
         }
     }
 
-    private static void readFromInput (Scanner scanner){
+    // Lies Eingabe (entweder aus Konsole oder aus File) und übergebe es der BlockingQueue
+    private static void readFromInputStream(Scanner scanner){
         if(scanner.hasNext()) {
             String message = scanner.nextLine();
             if (validInputCheck(message)) {
@@ -97,17 +103,19 @@ public class Sensor {
         }
     }
 
+    // Prüfen der Eingabe nach gewissen Kriterien zur Sicherstellung der Robustheit des Systems
     private static boolean validInputCheck (String input) {
         // SHUTDOWN_KEY "QUIT" soll an Server weitergereicht werden als Signal, den Client-Socket geordnet zu schließen
         if (input.equals(SHUTDOWN_KEY))
             return true;
 
-        // Falls keine korrekte Angabe des Sensors, return false und warte auf nächste Zeile
+        // Falls keine korrekte Angabe des Sensorrichtung ("LINKS" oder "RECHTS"), return false und warte auf nächste Zeile
         if (!input.contains("LINKS") && !input.contains("RECHTS")){
             SYSTEM_LOGGER.error("Ungültige Eingabe der Sensorrichtung --> Lese nächste Zeile");
             return false;
         }
-        // Falls JSON Syntax Fehler, return false und warte danach auf nächste Zeile
+        // Falls JSON Syntax Fehler, return false und warte danach auf nächste Zeile:
+        // Eingabe wird erst gar nicht an BlockingQueue bzw. Server weitergereicht
         // Falls JSON Syntax korrekt, return true und reiche danach input an BlockingQueue weiter
         Gson gson = new Gson();
         try {
@@ -119,6 +127,7 @@ public class Sensor {
         }
     }
 
+    // Geordnetes Herunterfahren des Sensors/Clients, sodass Server nicht abstürzt
     private static void shutdownSensor(Socket socket, Sensor sensor){
         try {
             socket.close();
--- 01reactive/src/main/java/de/thro/inf/reactive/Server.java
+++ 01reactive/src/main/java/de/thro/inf/reactive/Server.java
@@ -18,14 +18,16 @@ import java.util.concurrent.LinkedBlockingQueue;
 public class Server {
 
     private static final Logger SYSTEM_LOGGER = Logger.getLogger("systemLogger");
-
     private static BlockingQueue<Ereignis> incoming = new LinkedBlockingQueue<>();
     private static Executor exec = Executors.newCachedThreadPool();
+    // Hier wird Config-Art festgelegt: new FromConsole() oder new FromFile()
+    // Config liest mithilfe von Properties-File nötige Konfigurationsinfos aus
     private static IConfiguration config = new FromConsole();
 
     public static void main(String[] args) {
-        Thread serverSocketThread = new Thread(() -> provideServerSocket());
-        serverSocketThread.start();
+        //Thread serverSocketThread = new Thread(() -> provideServerSocket());
+        //serverSocketThread.start();
+        exec.execute(() -> provideServerSocket());
 
         Mitarbeiterverwaltung mitarbeiterverwaltung = Mitarbeiterverwaltung.getMitarbeiterverwaltung();
 
@@ -49,31 +51,30 @@ public class Server {
                 SYSTEM_LOGGER.info("Warten auf Client-Anfrage ... ");
                 Socket client = server.accept();
                 SYSTEM_LOGGER.info("Neuer Client akzeptiert");
-                exec.execute(() -> readFromSocket(client));
+                exec.execute(() -> readFromClientSocket(client));
             }
-
         } catch (IOException ex) {
             SYSTEM_LOGGER.error(ex.getMessage());
         }
         SYSTEM_LOGGER.info("Server beendet");
     }
 
-    public static void readFromSocket(Socket client) {
+    public static void readFromClientSocket(Socket client) {
         try(InputStream inputStream = client.getInputStream()) {
             Gson gson = new Gson();
             Scanner scanner = new Scanner(inputStream);
 
             while (true) {
                 String message = scanner.nextLine();
-
+                // Bei Senden von SHUTDOWN_KEY: Schließe Client-Socket geordnet
                 if (message.equals("QUIT")){
                     client.close();
                     return;
                 }
+                // Reiche Ereignis-Objekt an BlockingQueue weiter (Prüfung auf valides JSON Format bereits im Client)
                 Ereignis ereignis = gson.fromJson(message, Ereignis.class);
                 incoming.put(ereignis);
             }
-
         } catch (IOException | InterruptedException ex) {
             SYSTEM_LOGGER.error(ex.getMessage());
         }
