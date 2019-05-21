69da6fea1c1e3162d381c96fcab5a3d37b810208
Karl Herzog <karl.m.herzog@stud.fh-rosenheim.de>
2019-05-21 15:15:27 +0200

    Uebung_06 Client erzeugen

---
+++ Uebungen_und_Katas/Uebung_06/src/main/java/de/thro/inf/kunde/KundeRepository.java
@@ -0,0 +1,8 @@
+package de.thro.inf.kunde;
+
+import de.thro.inf.kunde.Kunde;
+import org.springframework.data.repository.CrudRepository;
+
+public interface KundeRepository extends CrudRepository<Kunde, Long> {
+
+}
