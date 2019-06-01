6bc0b32cfb078bce7529fa1eb2fda1bce05e94e9
Karl Herzog <Karl.M.Herzog@stud.fh-rosenheim.de>
2019-06-01 16:56:43 +0200

    MitgliedController hinzufügen

--- 02restful/src/main/java/de/thro/vv/kleiderkreisel/MitgliedController.java
+++ 02restful/src/main/java/de/thro/vv/kleiderkreisel/MitgliedController.java
@@ -6,13 +6,12 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 import springfox.documentation.spring.web.scanners.MediaTypeReader;
 
 import java.awt.*;
@@ -23,24 +22,28 @@ import java.util.List;
         produces = MediaType.APPLICATION_JSON_VALUE,
         basePath = "api/v1")
 @RestController
-@RequestMapping(value = "api/v1")
+@RequestMapping(value = "api/v1")  // Versionierung
 public class MitgliedController {
 
     private MitgliedRepository mrepo;
+
     @Autowired
     public MitgliedController (MitgliedRepository mrepo){
         this.mrepo = mrepo;
     }
 
-    @ApiOperation( value = "Finde Mitglied nach ID",
+    @ApiOperation(
+            value = "Finde Mitglied über ID",
             response = Mitglied.class)
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Mitglied gefunden"),
-            @ApiResponse(code = 404, message = "Mitglied nicht gefunden")})
-    @RequestMapping( value = "mitglieder/{id}",
+    @ApiResponses(
+            value = {
+                    @ApiResponse(code = 200, message = "Mitglied gefunden"),
+                    @ApiResponse(code = 404, message = "Mitglied nicht gefunden")})
+    @RequestMapping(
+            value = "mitglieder/{id}",
             method = RequestMethod.GET,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity findMitgliedById (@PathVariable("id") String id) {
+    public ResponseEntity<?> findMitgliedById (@PathVariable("id") String id) {
         Mitglied mitglied = mrepo.findById(Long.parseLong(id)).orElse(null);
 
         if (mitglied != null) {
@@ -50,21 +53,108 @@ public class MitgliedController {
         }
     }
 
-    // List<Mitglied>.class nicht möglich -> Swagger zeigt in Vorschau leere Liste an
-    @ApiOperation(value = "Finde alle Mitglieder", response = List.class)
+
+    @ApiOperation(
+            value = "Finde alle Mitglieder",
+            response = List.class)
     @ApiResponses(
-            @ApiResponse(code = 200, message = "Alles Mitglieder aufgelistet")
+            @ApiResponse(code = 200, message = "Alle Mitglieder aufgelistet")
     )
-    @RequestMapping(value = "mitglieder",
-    method = RequestMethod.GET,
-    produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(
+            value = "mitglieder",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<Mitglied>> findAllMitglieder(){
         Iterable<Mitglied> mitgliedIterable = mrepo.findAll();
-        List<Mitglied> mitglieder = new LinkedList<>();
-        mitgliedIterable.forEach(mitglied -> mitglieder.add(mitglied));
+        List<Mitglied> mitgliederList = new LinkedList<>();
+        mitgliedIterable.forEach(mitglied -> mitgliederList.add(mitglied));
 
-    return new ResponseEntity<>(mitglieder, HttpStatus.OK);
+        return new ResponseEntity<>(mitgliederList, HttpStatus.OK);
     }
 
 
+    @ApiOperation(
+            value = "Lege neues Mitglied an",
+            response = Mitglied.class)
+    @ApiResponses(
+            @ApiResponse(code = 201, message = "Neues Mitglied angelegt")
+    )
+    @RequestMapping(
+            value = "mitglieder",
+            method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<?> createNewMitglied (@RequestBody Mitglied mitglied,
+                                                UriComponentsBuilder uriComponentsBuilder) {
+        mrepo.save(mitglied);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setLocation(uriComponentsBuilder
+                .path("api/v1/mitglieder/{id}")
+                .buildAndExpand(mitglied.getNummer()).toUri());
+        return new ResponseEntity<String>(httpHeaders, HttpStatus.CREATED);
+    }
+
+
+    @ApiOperation(
+            value = "Ändere Mitglied",
+            response = Mitglied.class)
+    @ApiResponses(
+            @ApiResponse(code = 200, message = "Mitglied geändert"))
+    @RequestMapping(
+            value = "mitglieder/{id}",
+            method = {RequestMethod.PUT, RequestMethod.PATCH},
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<?> modifyMitglied(@PathVariable("id") String id,
+                                            @RequestBody Mitglied mitgliedUpdate{
+        Mitglied existingMitglied = mrepo.findById(Long.parseLong(id))
+                .orElse(null);
+        if (existingMitglied == null){
+            return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
+        }
+        if (existingMitglied.getVersion() > mitgliedUpdate.getVersion()){
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+        // ID beibt unverändert
+        existingMitglied.setNachname(mitgliedUpdate.getNachname());
+        existingMitglied.setVorname(mitgliedUpdate.getVorname());
+        existingMitglied.setEmail(mitgliedUpdate.getEmail());
+        existingMitglied.setPostAdresse(mitgliedUpdate.getAdresse());
+        existingMitglied.setKontostand(mitgliedUpdate.getKontostand());
+        existingMitglied.setPassword(mitgliedUpdate.getPassword());
+        existingMitglied.setFoto(mitgliedUpdate.getFoto());
+
+        mrepo.save(existingMitglied);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    
+    @ApiOperation(
+            value = "Lösche ein Mitglied",
+            response = Mitglied.class)
+    @ApiResponses(
+            @ApiResponse(code = 202, message = "Mitglied gelöscht"))
+    @RequestMapping(
+            value = "mitglieder/{id}",
+            method = RequestMethod.DELETE,
+            consumes = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<Void> deleteMitglied (@PathVariable("id") String id,
+                                                @RequestBody Mitglied mitglied){
+        Mitglied existingMitglied = mrepo.findById(Long.parseLong(id)).orElse(null);
+        if (existingMitglied == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        if(existingMitglied.getVersion() > mitglied.getVersion()){
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+        if(!existingMitglied.getNummer().equals(mitglied.getNummer())){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        mrepo.delete(existingMitglied);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
